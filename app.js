6ce73efe9de358624cb620536575715b26e219a6
Shatailo1987 <shataylo.vrdp@gmail.com>
2026-03-04 11:55:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,32 +23,7 @@ const authScreen = document.getElementById("authScreen");
 const app = document.getElementById("app");
 const authError = document.getElementById("authError");
 
-document.getElementById("registerBtn").onclick = async () => {
-  try {
-    await createUserWithEmailAndPassword(auth, email.value, password.value);
-  } catch (error) {
-    authError.innerText = error.message;
-  }
-};
-
-document.getElementById("loginBtn").onclick = async () => {
-  try {
-    await signInWithEmailAndPassword(auth, email.value, password.value);
-  } catch (error) {
-    authError.innerText = error.message;
-  }
-};
-
-document.getElementById("logoutBtn").onclick = () => signOut(auth);
-
-onAuthStateChanged(auth, user => {
-  if (user) {
-    authScreen.style.display = "none";
-    app.style.display = "block";
-    initApp(user);
-  } else {
-    authScreen.style.display = "block";
-    app.style.display = "none";
+initAuth(auth, initApp);
   }
 });
 
