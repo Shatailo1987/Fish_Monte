2600436350f0dcd89463159cda6a252e7206df44
Shatailo1987 <shataylo.vrdp@gmail.com>
2026-03-04 11:48:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ function initApp(user) {
   document.getElementById("logoutBtn").onclick = () => signOut(auth);
 document.getElementById("tabSales").onclick =
 () => renderSales(content, buyersRef, salesRef, getDocs, addDoc, onSnapshot);
-  document.getElementById("tabExpenses").onclick =
+ document.getElementById("tabExpenses").onclick =
 () => renderExpenses(content);
 
   const content = document.getElementById("content");
