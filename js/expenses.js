97e51dbff3203a1d1b1bfd27f7da7620af306fce
Shatailo1987 <shataylo.vrdp@gmail.com>
2026-03-04 11:39:57 +0200

    Update expenses.js

--- js/expenses.js
+++ js/expenses.js
@@ -1,1 +1,38 @@
+export function renderExpenses(content) {
 
+content.innerHTML = `
+<h2>Витрати</h2>
+
+<select id="expenseCategory">
+<option value="">-- Оберіть категорію --</option>
+<option>Корм</option>
+<option>Зарибок</option>
+<option>Пальне</option>
+<option>Зарплата Рибаки</option>
+<option>Ремонт</option>
+<option>Інше</option>
+</select>
+
+<div id="dynamicFields"></div>
+
+<button id="saveExpense">Зберегти</button>
+
+<hr>
+
+<h3>Історія витрат</h3>
+<div id="expensesList"></div>
+
+<div>
+<b>ЗАГАЛЬНІ ВИТРАТИ: <span id="totalExpenses">0</span> грн</b>
+</div>
+
+<div>
+<b>Компенсація пального рибалкам: <span id="totalFuel">0</span> грн</b>
+</div>
+
+<div>
+<b>Зарплата рибалкам: <span id="totalSalary">0</span> грн</b>
+</div>
+`;
+
+}
