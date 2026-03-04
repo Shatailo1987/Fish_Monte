646044f144812bc69181c621d953846b0afefa5e
Shatailo1987 <shataylo.vrdp@gmail.com>
2026-03-04 11:50:43 +0200

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,71 @@
+export function initAuth(auth, initApp) {
+
+const {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+onAuthStateChanged,
+signOut
+} = window.firebaseFns;
+
+const authScreen = document.getElementById("authScreen");
+const app = document.getElementById("app");
+const authError = document.getElementById("authError");
+
+document.getElementById("registerBtn").onclick = async () => {
+
+try {
+
+await createUserWithEmailAndPassword(
+auth,
+email.value,
+password.value
+);
+
+} catch (error) {
+
+authError.innerText = error.message;
+
+}
+
+};
+
+document.getElementById("loginBtn").onclick = async () => {
+
+try {
+
+await signInWithEmailAndPassword(
+auth,
+email.value,
+password.value
+);
+
+} catch (error) {
+
+authError.innerText = error.message;
+
+}
+
+};
+
+document.getElementById("logoutBtn").onclick =
+() => signOut(auth);
+
+onAuthStateChanged(auth, user => {
+
+if (user) {
+
+authScreen.style.display = "none";
+app.style.display = "block";
+
+initApp(user);
+
+} else {
+
+authScreen.style.display = "block";
+app.style.display = "none";
+
+}
+
+});
+
+}
