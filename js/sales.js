b802d0a2e4fa6197e007f256b3620c2b5561a05c
Shatailo1987 <shataylo.vrdp@gmail.com>
2026-03-05 10:03:26 +0200

    Update sales.js

--- js/sales.js
+++ js/sales.js
@@ -65,7 +65,7 @@ const newPhone = document.getElementById("newPhone");
 
 const fishType = document.getElementById("fishType");
 const weightInput = document.getElementById("weightInput");
-const addWeight = document.getElementById("addWeight");
+const addWeightBtn = document.getElementById("addWeight");
 
 const weightsList = document.getElementById("weightsList");
 const totalKg = document.getElementById("totalKg");
