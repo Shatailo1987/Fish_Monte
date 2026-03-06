fb1a88c990ba7aca36a63779870c7efff5dc70d5
Shatailo1987 <shataylo.vrdp@gmail.com>
2026-03-06 16:04:51 +0200

    Update sales.js

--- js/sales.js
+++ js/sales.js
@@ -353,12 +353,6 @@ filterFrom = dateFrom.value ? new Date(dateFrom.value) : null;
 filterTo = dateTo.value ? new Date(dateTo.value) : null;
 
 renderSalesHistory();
-  
-${data.buyerName}
-<span style="color:#888;cursor:pointer"
-onclick="navigator.clipboard.writeText('${data.buyerPhone}')">
-(${data.buyerPhone})
-</span>
 
 };
 
